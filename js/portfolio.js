13c76f557504ddabc26c05b73847bf5f73228396
emilyyujeong <118698609+emilyyujeong@users.noreply.github.com>
2023-04-06 20:07:05 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,70 @@
+$(function () {
+
+    var fullpageOption = {
+        anchors: [
+            'main',
+            'portfolio01',
+            'portfolio02',
+            'portfolio03',
+            'portfolio04',
+            'portfolio05',
+            'profile'
+        ],
+
+        //navigation: true,
+        onLeave: function (origin, destination, direction) {
+            console.log(origin, destination, direction);
+            $('.gnb li')
+                .eq(destination - 1)
+                .addClass('on')
+                .siblings()
+                .removeClass('on');
+            $('.section').eq(destination - 1)
+                .addClass('on')
+                .siblings()
+                .removeClass('on');
+        },
+
+        afterRender: function () {
+            $('.gnb li')
+                .eq(0)
+                .addClass('on');
+            $('.section')
+                .eq(0)
+                .addClass('on');
+
+        }
+
+    }
+
+
+    $('.main').fullpage(fullpageOption);
+
+
+    function gnb() {
+        var gnbHeight = $('.gnb').height();
+        $('.gnb').css({
+            transform: `translate(0, ${gnbHeight / 2})`
+        })
+    }
+
+    gnb()
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on');
+        $('.cover').toggleClass('on');
+        $('.header').toggleClass('on');
+    });
+
+    $('.cover li').on('click', function () {
+        $('.cover').removeClass('on');
+        $('.mopen').removeClass('on');
+        $('.header').removeClass('on');
+    });
+
+    $('.cover').on('wheel', function (e) {
+        e.stopPropagation()
+    });
+
+
+})
